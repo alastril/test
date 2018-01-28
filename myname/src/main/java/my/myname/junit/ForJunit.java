a1828586937f9bdd7dee7cc2411b0e61128d2656
alastril <alastril@i.ua>
2018-01-28 16:17:01 +0200

    junit, 4-or tests for random method

--- myname/src/main/java/my/myname/junit/ForJunit.java
+++ myname/src/main/java/my/myname/junit/ForJunit.java
@@ -4,17 +4,21 @@ import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
-import my.myname.entity.Zoo;
 
 public class ForJunit {
 
 	
-	public Set<Integer> getMyName(int count){
+	public Set<Integer> getRandomByCount(Integer count, Integer range) throws IllegalArgumentException{
+			if(range<count || range<0)throw new IllegalArgumentException("Error: numberValueLimit < count; Need count>numberValueLimit!");
 			Set<Integer>  setUnique = new TreeSet<>();
 			Random r = new Random();
-//			for(){
-//				
-//			}
+			for(int i=0; i<count; i++) {
+				Integer current = r.nextInt(range);
+				while(setUnique.contains(current)) {
+					current = r.nextInt(range);
+				}
+				setUnique.add(current);
+			}
 		return setUnique;
 	}
 	
--- myname/src/test/java/myname/ForJunitTest.java
+++ myname/src/test/java/myname/ForJunitTest.java
@@ -1,5 +1,35 @@
 package myname;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import my.myname.junit.ForJunit;
+
 public class ForJunitTest {
 
+	
+	@Test
+	public void testRandomCount() {
+		ForJunit ju = new ForJunit();
+		assertTrue(ju.getRandomByCount(10,200).size()>0); 
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testRandomRangeIllegalArg() {
+		ForJunit ju = new ForJunit();
+		System.out.println(ju.getRandomByCount(100,10)); 
+	}
+	
+	@Test
+	public void testRandomCountNotNull() {
+		ForJunit ju = new ForJunit();
+		assertTrue(ju.getRandomByCount(0,0).isEmpty()); 
+	}
+	
+	@Test
+	public void testRandomEqualsCountElementsToFirstArg() {
+		ForJunit ju = new ForJunit();
+		assertTrue(ju.getRandomByCount(10,20).size()==10); 
+	}
 }
