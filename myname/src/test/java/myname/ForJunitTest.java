74fcd06ea4c044cfd7627620519a6e63e1546967
alastril <alastril@i.ua>
2018-03-08 19:40:12 +0200

    add java test call oauth2

--- myname/src/test/java/myname/ForJunitTest.java
+++ myname/src/test/java/myname/ForJunitTest.java
@@ -4,16 +4,30 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 
+import my.myname.crud_spr_data.entity.Animals;
+import my.myname.crud_spr_data.entity.Food;
+import my.myname.crud_spr_data.entity.Zoo;
 import my.myname.junit.ForJunit;
+import my.myname.mvc.ControllerZoo;
+import my.myname.mvc.MarshUnmarsh;
 
 public class ForJunitTest {
-
+	private static final Logger log = Logger.getLogger(ForJunitTest.class);
 	
 	@Test
 	public void testRandomCount() {
@@ -39,24 +53,38 @@ public class ForJunitTest {
 		assertTrue(ju.getRandomByCount(10,20).size()==10); 
 	}
 	
-//	@Test
+	@Test
 	public void testOauth() {
-		 ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
-	        resourceDetails.setUsername("pasha");
-	        resourceDetails.setPassword("baseSecret");
-	        resourceDetails.setAccessTokenUri("http://localhost:8080/myname/restful/oauth/token");
-	        resourceDetails.setClientId("pasha");
-	        resourceDetails.setClientSecret("123456");
-	        resourceDetails.setGrantType("password");
-	        resourceDetails.setScope(Arrays.asList("read", "write"));
+		ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
+		resourceDetails.setUsername("pasha");
+		resourceDetails.setPassword("baseSecret");
+		resourceDetails.setAccessTokenUri("http://localhost:8080/myname/restful/oauth/token");
+		resourceDetails.setClientId("pasha");
+		resourceDetails.setClientSecret("123456");
+		resourceDetails.setGrantType("password");
+		resourceDetails.setScope(Arrays.asList("read", "write"));
 
-	        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+		DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+		try {
+			OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, clientContext);
+			Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+			jaxb2Marshaller.setClassesToBeBound(MarshUnmarsh.class, Zoo.class, Animals.class, Food.class);
+			MarshallingHttpMessageConverter httpMessageConverter = new MarshallingHttpMessageConverter();
+			httpMessageConverter.setMarshaller(jaxb2Marshaller);
+			httpMessageConverter.setUnmarshaller(jaxb2Marshaller);
+			restTemplate.setMessageConverters(Arrays.asList(httpMessageConverter));
 
-	        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, clientContext);
-	        restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter()));
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_XML);
+			HttpEntity<Object> requestEntity = new HttpEntity<Object>(null, headers);
 
-	        final String greeting = restTemplate.getForObject("http://localhost:8080/myname/restful/zoo/xmlzoo", String.class);
+			final ResponseEntity<MarshUnmarsh> xmlzoo = restTemplate.exchange(
+					"http://localhost:8080/myname/restful/zoo/xmlzoo", HttpMethod.GET, requestEntity,
+					MarshUnmarsh.class);
 
-	        System.out.println(greeting);
+			System.out.println(xmlzoo);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
 	}
 }
